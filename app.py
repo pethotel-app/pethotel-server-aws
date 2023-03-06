b1afee8b4ae9a2ecdd00c397a655ab5fcb98f537
leobaek <120348541+leobaek@users.noreply.github.com>
2023-03-06 14:17:30 +0900

    merge
    merge

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from config import Config
 from resource.benefit import CouponSearchResource, CouponUseResource, PointAddResource, PointSearchResource
 from resource.favorite import FavoriteListResource, FavoriteResource
 from resource.hotel import HotelInfoResource, HotelSearchRankResource, HotelSearchResource
+from resource.pet import PetListResource, PetResource
+from resource.reservation import ReservationResource
+from resource.review import ReviewListResource
 
 from resource.user import UserChangePasswordResource, UserIdSearchResource, UserImageResource, UserInfoResource, UserLoginResource, UserLogoutResource, UserPasswordSearchResource, UserRegisterResource, jwt_blacklist
 
@@ -26,7 +29,6 @@ api = Api(app)
 api.add_resource(UserRegisterResource, '/user/register')
 api.add_resource(UserLoginResource, '/user/login')
 api.add_resource(UserLogoutResource, '/user/logout')
-
 api.add_resource(UserInfoResource, '/user/info')
 api.add_resource(UserImageResource, '/user/profile')
 api.add_resource(PointSearchResource, '/benefit/point')
@@ -35,21 +37,21 @@ api.add_resource(CouponSearchResource,'/benefit/coupon')
 api.add_resource(CouponUseResource, '/benefit/coupon/<int:couponId>')
 api.add_resource(HotelInfoResource, '/hotel/<int:hotelId>')
 api.add_resource(HotelSearchResource, '/hotel/searchList/<string:keyword>')
-
-
 api.add_resource(FavoriteResource, '/favorite/<int:hotelId>')
 api.add_resource(FavoriteListResource, '/favorite')
-
 api.add_resource(HotelSearchResource, '/hotel/search')
 api.add_resource(HotelSearchRankResource, '/hotel/search/rank')
-
-
-
-
-
 api.add_resource(UserIdSearchResource, '/user/id/search')
 api.add_resource(UserPasswordSearchResource, '/user/password/search')
 api.add_resource(UserChangePasswordResource, '/user/change/password')
+# 펫
+api.add_resource(PetListResource, '/pets/')
+api.add_resource(PetResource, '/pets/<int:petId>')
+
+# 리뷰
+api.add_resource(ReviewListResource, '/review/<int:hotelId>')
+# 예약
+api.add_resource(ReservationResource, '/reservation')
 
 if __name__ == '__main__' :
     app.run()
