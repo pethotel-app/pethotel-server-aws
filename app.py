48ab8adbb7f86baadd5a4e4be3f0557e05304192
leobaek <120348541+leobaek@users.noreply.github.com>
2023-03-06 14:07:59 +0900

    충돌 확인
    충돌 확인

--- app.py
+++ app.py
@@ -1,229 +1,15 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
 from config import Config
-<<<<<<< HEAD
-
-from resource.favorite import FavoriteListResource, FavoriteResource
-from resource.hotel import HotelSearchRankResource, HotelSearchResource
-
-from resource.pet import PetListResource, PetResource
-from resource.reservation import ReservationResource
-from resource.review import ReviewListResource
-
-=======
 from resource.benefit import CouponSearchResource, CouponUseResource, PointAddResource, PointSearchResource
-from resource.hotel import HotelInfoResource, HotelSearchResource
->>>>>>> BMW
+from resource.favorite import FavoriteListResource, FavoriteResource
+from resource.hotel import HotelInfoResource, HotelSearchRankResource, HotelSearchResource
+
 from resource.user import UserChangePasswordResource, UserIdSearchResource, UserImageResource, UserInfoResource, UserLoginResource, UserLogoutResource, UserPasswordSearchResource, UserRegisterResource, jwt_blacklist
 
-# branch test
-# branch test
-# branch test
-# branch test
-# branch test
+
 app = Flask(__name__)
 
 app.config.from_object(Config)
@@ -240,17 +26,7 @@ api = Api(app)
 api.add_resource(UserRegisterResource, '/user/register')
 api.add_resource(UserLoginResource, '/user/login')
 api.add_resource(UserLogoutResource, '/user/logout')
-<<<<<<< HEAD
 
-api.add_resource(UserIdSearchResource, '/user/id/search')
-api.add_resource(UserPasswordSearchResource, '/user/password/search')
-api.add_resource(UserChangePasswordResource, '/user/change/password')
-
-=======
-api.add_resource(UserIdSearchResource, '/user/IdSearch')
-api.add_resource(UserPasswordSearchResource, '/user/PasswordSearch')
-api.add_resource(UserChangePasswordResource, '/user/ChangePassword')
->>>>>>> BMW
 api.add_resource(UserInfoResource, '/user/info')
 api.add_resource(UserImageResource, '/user/profile')
 api.add_resource(PointSearchResource, '/benefit/point')
@@ -261,23 +37,19 @@ api.add_resource(HotelInfoResource, '/hotel/<int:hotelId>')
 api.add_resource(HotelSearchResource, '/hotel/searchList/<string:keyword>')
 
 
-
 api.add_resource(FavoriteResource, '/favorite/<int:hotelId>')
 api.add_resource(FavoriteListResource, '/favorite')
 
 api.add_resource(HotelSearchResource, '/hotel/search')
 api.add_resource(HotelSearchRankResource, '/hotel/search/rank')
 
-# 펫
-api.add_resource(PetListResource, '/pets/')
-api.add_resource(PetResource, '/pets/<int:petId>')
 
-# 리뷰
-api.add_resource(ReviewListResource, '/review/<int:hotelId>')
 
-# 예약
-api.add_resource(ReservationResource, '/reservation')
 
+
+api.add_resource(UserIdSearchResource, '/user/id/search')
+api.add_resource(UserPasswordSearchResource, '/user/password/search')
+api.add_resource(UserChangePasswordResource, '/user/change/password')
 
 if __name__ == '__main__' :
     app.run()
--- resource/hotel.py
+++ resource/hotel.py
@@ -1,32 +1,19 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> BMW
 from flask import request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask_restful import Resource
 from mysql_connection import get_connection
 from mysql.connector import Error
 
-<<<<<<< HEAD
 class HotelSearchResource(Resource) :
     # 검색한 호텔 리스트 가져오기
     def get(self) :
         keyword = request.args.get('keyword')
-=======
-# 호텔 검색 리스트
-class HotelSearchResource(Resource) :
-    @jwt_required(optional=True)
-    def get(self,keyword) :
-
->>>>>>> BMW
         offset = request.args.get('offset')
         limit = request.args.get('limit')
 
         try :
             connection = get_connection()
 
-<<<<<<< HEAD
             query = '''select f.hotelId, h.title, h.imgUrl, ifnull(avg(r.rating),0) as avg, ifnull(count(r.hotelId),0) as cnt,
                     if(f.userId is null, 0, 1) as 'favorite'
                     from yh_project_db.hotel h
@@ -45,26 +32,6 @@ class HotelSearchResource(Resource) :
             i = 0
             for row in result_list :
                 result_list[i]['avg'] = float(row['avg'])
-=======
-            query ='''select * from hotel
-            where addr LIKE %s
-            LIMIT '''+offset+''','''+limit+''';'''
-
-            record = (f"%{keyword}%",)
-
-            cursor = connection.cursor(dictionary=True)
-
-            cursor.execute(query,record)
-            resultList = cursor.fetchall()
-
-            if len(resultList) == 0 :
-                 return {"error": "검색 결과가 없습니다."}, 404
-
-            i = 0
-            for row in resultList :
-                resultList[i]['longtitude'] = float(row['longtitude'])
-                resultList[i]['latitude'] = float(row['latitude'])
->>>>>>> BMW
                 i = i + 1
 
             cursor.close()
@@ -75,21 +42,10 @@ class HotelSearchResource(Resource) :
             cursor.close()
             connection.close()
 
-<<<<<<< HEAD
             return {"error" : str(e)}, 500
 
         return {"result" : "success", "items" : result_list, "count" : len(result_list)}, 200
     
-    # 검색어 저장
-    def put(self) :
-        keyword = request.args.get('keyword')
-=======
-            return {'error' : str(e) } , 500
-
-            
-
-
-        return {'result' : 'success' , 'hotel' : resultList}
 
 
 
@@ -105,12 +61,60 @@ class HotelInfoResource(Resource) :
     @jwt_required(optional=True)
     def get(self, hotelId) :
 
->>>>>>> BMW
 
         try :
             connection = get_connection()
 
-<<<<<<< HEAD
+            query = '''select h.id,title,addr,longtitude,latitude,tel,naverUrl, small,medium,large
+                    from hotel h
+                    left join price p
+                    on h.id = p.hotelId
+                    where h.id = %s;'''
+
+
+            record = (hotelId,)
+
+            cursor = connection.cursor(dictionary=True)
+
+            cursor.execute(query,record)
+
+            resultList = cursor.fetchall()
+            
+            if resultList[0]['id'] is None :
+                return{'error' : '잘못된 호텔 아이디 입니다.'} , 400
+
+            i = 0
+            for row in resultList :
+                resultList[i]['longtitude'] = float(row['longtitude'])
+                resultList[i]['latitude'] = float(row['latitude'])
+                i = i + 1
+
+
+            cursor.close()
+            connection.close()
+
+
+
+        except Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+
+            return {'error' : str(e) } , 500
+
+            
+
+
+        return {'result' : 'success' , 'hotel' : resultList[0]}
+    
+
+     # 검색어 저장
+    def put(self) :
+        keyword = request.args.get('keyword')
+
+        try :
+            connection = get_connection()
+
             query = '''insert into yh_project_db.keyword(keyword)
                     values(%s);'''
 
@@ -163,58 +167,11 @@ class HotelSearchRankResource(Resource) :
             cursor.close()
             connection.close()
 
-=======
-            query = '''select h.id,title,addr,longtitude,latitude,tel,naverUrl, small,medium,large
-                    from hotel h
-                    left join price p
-                    on h.id = p.hotelId
-                    where h.id = %s;'''
-
-
-            record = (hotelId,)
-
-            cursor = connection.cursor(dictionary=True)
-
-            cursor.execute(query,record)
-
-            resultList = cursor.fetchall()
-            
-            if resultList[0]['id'] is None :
-                return{'error' : '잘못된 호텔 아이디 입니다.'} , 400
-
-            i = 0
-            for row in resultList :
-                resultList[i]['longtitude'] = float(row['longtitude'])
-                resultList[i]['latitude'] = float(row['latitude'])
-                i = i + 1
-
-
-            cursor.close()
-            connection.close()
-
-
-
->>>>>>> BMW
         except Error as e :
             print(e)
             cursor.close()
             connection.close()
 
-<<<<<<< HEAD
             return {"error" : str(e)}, 500
 
         return {"result" : "success", "items" : result_list, "count" : len(result_list)}, 200
-=======
-            return {'error' : str(e) } , 500
-
-            
-
-
-        return {'result' : 'success' , 'hotel' : resultList[0]}
-
-
-
-
-
-
->>>>>>> BMW
